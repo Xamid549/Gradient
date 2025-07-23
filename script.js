3430fc439920afdfa180313470c97a0162034390
PPP <xamidramazanob@gmail.com>
2025-07-23 16:14:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+ let typed = new Typed('#ism', {
+      strings: ['TEXT GRADIENTS'],
+      typeSpeed: 100,
+      loop:true
+    });
+ let typed2 = new Typed('#p', {
+      strings: ['', 'Make your headlines stand out'],
+      typeSpeed: 100,
+      
+    });
